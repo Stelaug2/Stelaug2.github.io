239c51776e338268d0b8ee11f2177b1443829f89
Elida <89187075+Stelaug2@users.noreply.github.com>
2021-08-26 08:54:14 +0200

    Update java.js

--- java.js
+++ java.js
@@ -4,13 +4,15 @@ function startTime() {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
+    h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('tid').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
   }
   
+  // add zero in front of numbers < 10
   function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};  
     return i;
   }
