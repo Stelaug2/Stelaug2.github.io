119a903b0aad1f9b499ffa9e834bd88d055f01f8
Elida <89187075+Stelaug2@users.noreply.github.com>
2022-03-10 10:23:26 +0100

    aeøguoslhj

---
+++ intro js/oppgaver/arrays/matbutikk/matbutikk.js
@@ -0,0 +1,13 @@
+let varerEl = document.getElementById("varer");
+let handleKurvEl = document.getElementById("handleKurv");
+
+let varer = [
+    {navn: "Gulrot", Pris: 12.15, bilde: "mat_01.png", handlekurv: 0},
+    {navn: "Rødløk", Pris: 8.90, bilde: "mat_05.png", handlekurv: 0},
+    {navn: "Agurk", Pris: 10.40, bilde: "mat_08.png", handlekurv: 0},
+    {navn: "Paprika", Pris: 14.00, bilde: "mat_13.png", handlekurv: 0},
+    {navn: "Potet", Pris: 5.50, bilde: "mat_14.png", handlekurv: 0},
+    {navn: "Tomat", Pris: 8.30, bilde: "mat_19.png", handlekurv: 0}
+]
+
+let varerDiv = document.createElement("div");
