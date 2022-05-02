c706c4ae848a7297eac71b8b72f447de147721f2
Elida <89187075+Stelaug2@users.noreply.github.com>
2022-05-02 17:56:07 +0200

    noe

--- intro js/jrpg/jrpg.js
+++ intro js/jrpg/jrpg.js
@@ -3,23 +3,32 @@ let enemyDisplay = document.getElementById("enemy_display");
 let description = document.getElementById("battleDescription");
 let andersImage = document.getElementById("andersImage");
 
-let attackPressed = document.getElementById("attack");
+let attackButton = document.getElementById("attack");
+let counterButton = document.getElementById("counter");
+let healButton = document.getElementById("heal");
+
 let enemyHP = 20;
+let andersHP = 20;
+
+enemyDisplay.innerHTML = "HP: " + enemyHP;
+andersDisplay.innerHTML = "HP: " + andersHP;
 
 summonEnemy();
 
-enemyDisplay.innerHTML = "HP: " + enemyHP
-
-attackPressed.addEventListener("click", attackEnemy);
+attackButton.addEventListener("click", attackEnemy);
 
 function attackEnemy() {
+    disableButtons();
     let attackPower = randInt(3, 5);
     let newEnemyHP = enemyHP - attackPower;
     enemyHP = newEnemyHP;
     enemyDisplay.innerHTML = "HP: " + enemyHP;
     description.textContent = "You did " + attackPower + " damage";
+    if(enemyHP > 0) {
+        setTimeout(function() { attackAnders()}, 2000);
+    }
 
-    if(enemyHP <= 0) {
+    else if(enemyHP <= 0) {
         enemyDisplay.innerHTML = "HP: 0";
         description.textContent = "You defeated your enemy.";
         setTimeout(function() {description.textContent = "Oh no! Here comes a new one"; enemyHP = 20;
@@ -27,31 +36,40 @@ function attackEnemy() {
     }
 }
 
+function attackAnders() {
+    enableButtons();
+    console.log("enemy attacks!")
+    let attackPower = randInt(1, 3);
+    let newAndersHP = andersHP - attackPower;
+    andersHP = newAndersHP;
+    andersDisplay.innerHTML = "HP: " + andersHP;
+    description.textContent = "Your enemy did " + attackPower + " damage to you";
+}
+
 function summonEnemy() {
     let enemyImage = document.getElementById("enemyImage");
     console.log(enemyImage.src);
-    if(enemyImage.src === "http://127.0.0.1:5501/intro%20js/jrpg/jrpg1.html#") {
-        let randEnemy = randInt(1, 2);
-        if(randEnemy === 1) {
-            console.log("with all");
-            enemyImage.src = "bilder/karoliner_big.png";
-        }
-        else {
-            console.log("of the");
-            enemyImage.src = "bilder/redcoat_big.png";
-        }
-    }
-    else if(enemyImage.src === "http://127.0.0.1:5501/intro%20js/jrpg/bilder/karoliner_big.png") {
+    if(enemyImage.src === "http://127.0.0.1:5501/intro%20js/jrpg/bilder/karoliner_big.png") {
         enemyImage.src = "bilder/redcoat_big.png";
-        console.log("unga");
-        return;
+        console.log("red");
     } 
     else {
         enemyImage.src = "bilder/karoliner_big.png";
-        console.log("bunga");
+        console.log("blue");
     }
-    console.log(enemyImage);
     console.log(enemyImage.src);
+}
+
+function disableButtons() {
+    attackButton.disabled = true;
+    counterButton.disabled = true;
+    healButton.disabled = true;
+}
+
+function enableButtons() {
+    attackButton.disabled = false;
+    counterButton.disabled = false;
+    healButton.disabled = false;
 }
 
 function randInt(min, max) {
